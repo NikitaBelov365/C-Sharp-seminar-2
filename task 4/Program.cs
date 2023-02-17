0cd035db19718c6b6d9294544cc8a751d26a465a
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-18 01:26:19 +0300

    task 4 solved

---
+++ task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿// 4. Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+int Prompt(string msg)
+{
+    System.Console.WriteLine($"{msg}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt("Enter new number");
+int divider1 = Prompt("Enter first divider");
+int divider2 = Prompt("Enter second divider");
+if (number % divider1 == 0 & number % divider2 == 0)
+{
+    System.Console.WriteLine($"{number} is multiple to {divider1} and {divider2} at the same time!");
+}
+else
+{
+    System.Console.WriteLine($"{number} is not multiple to {divider1} and {divider2} at the same time.");
+}
