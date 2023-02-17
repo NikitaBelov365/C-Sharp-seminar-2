d7aa705d152892035506bff7ca36330f934c0a15
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-18 00:34:50 +0300

    task 2 solved

---
+++ task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Напишите программу, которая выводит случайное число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+int number = new Random().Next(9, 100);
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int Max = firstDigit;
+if(secondDigit>Max)
+{
+    Max=secondDigit;
+}
+System.Console.WriteLine($"Biggest digit from {number} is {Max}");
